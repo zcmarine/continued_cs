6613393f66f60bce2919116b244f2d87002e1270
Zach Marine <zmarine@squarespace.com>
2017-12-09 21:46:41 -0500

    Track median in infinitely long stream of ints

---
+++ incremental_median/incremental_median.py
@@ -0,0 +1,113 @@
+# A note on heaps:
+#     Heaps in Python are implemented via the heapq library
+#     Really, Python just uses lists for heaps, hence the operations
+#     are implemented by first instantiating a list:
+#         h = []
+#     then pushing to it:
+#         heapq.heappush(h, val)
+#     and popping from it:
+#         heapq.heappop(h)
+
+import heapq
+import logging
+
+# Note: if you ever want to override a module / library's logger levels
+# (e.g. you want to set this to DEBUG, you can find all loggers with:
+#     logging.Logger.manager.loggerDict
+# Then you can grab that logger and change its level:
+#     logging.getLogger('incremental_median').setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class Heap(object):
+    '''
+    Class-based implementation of a Heap since Python's
+    functional approach is a bit clunky
+    '''
+    def __init__(self, is_minheap=True):
+        self._h = []
+        self.sign = 1 if is_minheap else -1
+        self.kind = 'min' if is_minheap else 'max'
+        self.len = 0
+        logger.debug('Initializing {} heap'.format(self.kind))
+
+    @property
+    def h(self):
+        ''' Property provided to allow end user to view the heap; not used internally '''
+        return [self.sign * val for val in self._h]
+
+    def pop(self):
+        item = self.sign * heapq.heappop(self._h)
+        self.len -= 1
+        logger.debug('Popping item {} from {} heap'.format(item, self.kind))
+        return item
+
+    def push(self, val):
+        heapq.heappush(self._h, self.sign * val)
+        logger.debug('Pushing item {} to {} heap'.format(val, self.kind))
+        self.len += 1
+
+    def peek(self):
+        val = self.pop()
+        self.push(val)
+        logger.debug('Peeking at top item ({}) in {} heap'.format(val, self.kind))
+        return val
+
+
+class IncrementalMedian(object):
+    def __init__(self):
+        # Initialize min heap; used for all values larger than the median
+        self.minh = Heap()
+        # Initialize max heap; used for all values smaller than the median
+        self.maxh = Heap(is_minheap=False)
+
+    def get_median(self):
+        if self.minh.len == self.maxh.len:
+            logger.debug('Getting median for equally sized min and max heaps')
+            smallest_minh = self.minh.peek()
+            largest_maxh = self.maxh.peek()
+            return 0.5 * (smallest_minh + largest_maxh)
+        else:
+            logger.debug('Getting median from min heap (heaps are unequal in size)')
+            # minh should have the extra element
+            return float(self.minh.peek())
+
+    def insert(self, val):
+        ''' Insert a new value into the appropriate heap '''
+        # For simplicity's sake, just stick things onto minh and rebalance
+        self.minh.push(val)
+        self.rebalance()
+
+    def rebalance(self):
+        '''
+        Rebalance the min and max heaps so that their lengths are within 1 of each other
+        '''
+        # Since all elements go into minh, it will always be at least as large as maxh
+        imbalance = self.minh.len - self.maxh.len
+        # If there is an imbalance, we will move half the items to maxh, but if there is
+        # an extra item (i.e. imbalance is an odd number), then that extra item stays in
+        # minh, so here we do floor division
+        moves = imbalance // 2
+
+        for _ in range(moves):
+            item = self.minh.pop()
+            logger.debug('Moving item {} from min heap to max heap'.format(item))
+            self.maxh.push(item)
+
+        if self.maxh.len == 0:
+            return
+
+        # We moved things around, but make sure it still holds that
+        # max of self.minh is less than min of self.maxh; swap items
+        # until this is true
+        while self.minh.peek() < self.maxh.peek():
+            self.swap()
+
+    def swap(self):
+        minh_tip = self.minh.pop()
+        maxh_tip = self.maxh.pop()
+        logger.debug('Swapping minh item {} and maxh item {}'.format(minh_tip, maxh_tip))
+
+        self.maxh.push(minh_tip)
+        self.minh.push(maxh_tip)
---
+++ incremental_median/test_incremental_median.py
@@ -0,0 +1,141 @@
+import pytest
+
+import incremental_median
+
+
+@pytest.fixture(scope='function')
+def minh():
+    return incremental_median.Heap()
+
+
+@pytest.fixture(scope='function')
+def maxh():
+    return incremental_median.Heap(is_minheap=False)
+
+
+@pytest.fixture(scope='function')
+def im():
+    return incremental_median.IncrementalMedian()
+
+
+class TestHeap:
+    def test_init(self, minh):
+        assert minh.h == []
+        assert minh.sign == 1
+
+    def test_minheap_pop(self, minh):
+        minh._h = [1, 6, 3]
+        assert minh.pop() == 1
+
+    def test_minheap_push(self, minh):
+        minh.push(6)
+        minh.push(1)
+        minh.push(3)
+        assert minh._h == [1, 6, 3]
+
+    def test_minheap_peek(self, minh):
+        minh._h = [1, 6, 3]
+        assert minh.peek() == 1
+        assert minh._h == [1, 6, 3]
+
+    def test_maxheap_pop(self, maxh):
+        maxh._h = [-6, -1, -3]
+        assert maxh.pop() == 6
+
+    def test_maxheap_push(self, maxh):
+        maxh.push(1)
+        maxh.push(6)
+        maxh.push(3)
+        assert maxh._h == [-6, -1, -3]
+
+    def test_maxheap_peek(self, maxh):
+        maxh._h = [-8, -2, -5]
+        assert maxh.peek() == 8
+
+
+class TestIncrementalMedianFunctionality:
+    def test_init(self, im, minh):
+        assert im.minh.h == []
+        assert im.maxh.h == []
+
+    def test_get_median_even(self, im, minh, maxh):
+        minh.push(7)
+        maxh.push(4)
+        im.minh = minh
+        im.maxh = maxh
+        assert im.get_median() == 0.5 * (4 + 7)
+
+    def test_get_median_odd_and_empty(self, im, minh, maxh):
+        minh.push(4)
+        im.minh = minh
+        assert im.get_median() == 4.0
+
+    def test_get_median_odd_and_nonempty(self, im, minh, maxh):
+        minh.push(5)
+        minh.push(7)
+        im.minh = minh
+        maxh.push(1)
+        im.maxh = maxh
+        assert im.get_median() == 5.0
+
+    def test_swap(self, im, minh, maxh):
+        minh.push(1)
+        minh.push(9)
+        minh.push(7)
+        im.minh = minh
+        maxh.push(2)
+        im.maxh = maxh
+        im.swap()
+        assert im.minh.h == [2, 9, 7]
+        assert im.maxh.h == [1]
+
+    def test_rebalance(self):
+        pass
+
+    def test_insert(self):
+        pass
+
+
+class TestEndToEnd:
+    def test_one_insert(self, im):
+        im.insert(3)
+        assert im.get_median() == 3
+
+    def test_two_inserts(self, im):
+        im.insert(3)
+        im.insert(5)
+        assert im.get_median() == 4.0
+
+    def test_two_inserts_one_negative(self, im):
+        im.insert(3)
+        im.insert(-3)
+        assert im.get_median() == 0.0
+
+    def test_two_inserts_both_negative(self, im):
+        im.insert(-8)
+        im.insert(-3)
+        assert im.get_median() == -5.5
+
+    def test_swapping_occurs(self, im):
+        im.insert(11)
+        im.insert(70)
+        im.insert(10)
+        assert im.get_median() == 11
+
+    def test_multiple_swaps(self, im):
+        for i in (10, 7, 4, 2, 4, 4, 1):
+            im.insert(i)
+        assert im.get_median() == 4
+
+    @pytest.mark.parametrize('vals, expected', [
+        ([6, 18, 42, 34, 35, 33, 47, 22, 90, 39, 98, 57, 7, 13, 94, 11, 56, 90, 15, 60], 37.0),
+        ([17, 68, 37, 42, 88, 62, 91, 37, 68, 9, 26, 64, 82, 24, 85, 36, 51, 43, 68, 81], 56.5),
+        ([31, 65, 44, 11, 44, 38, 24, 32, 3, 60, 91, 81, 39, 39, 15, 68, 57, 8, 94, 9], 39.0),
+        ([57, 75, 22, 53, 52, 17, 71, 42, 43, 39, 18, 37, 31, 73, 95, 29, 27, 16, 89, 82], 42.5),
+        ([19, 0, 66, 42, 0, 93, 14, 75, 18, 8, 5, 56, 49, 89, 74, 56, 70, 33, 12, 60], 45.5),
+    ])
+    def test_bigger_data_sets(self, im, vals, expected):
+        ''' A variety of slightly larger data sets I tested against '''
+        for val in vals:
+            im.insert(val)
+        assert im.get_median() == expected
